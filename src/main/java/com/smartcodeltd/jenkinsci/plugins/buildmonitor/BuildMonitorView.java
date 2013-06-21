fcd859280ffc079564007acc70c90436453d43d4
jan-molak <jan.molak@gmail.com>
2013-06-21 23:55:55 +0100

    Very basic user interface displaying current state of the build jobs.

--- src/main/java/com/smartcodeltd/jenkinsci/plugins/buildmonitor/BuildMonitorView.java
+++ src/main/java/com/smartcodeltd/jenkinsci/plugins/buildmonitor/BuildMonitorView.java
@@ -6,7 +6,11 @@ import hudson.model.AbstractProject;
 import hudson.model.ListView;
 import hudson.model.TopLevelItem;
 import hudson.model.ViewDescriptor;
+import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.bind.JavaScriptMethod;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,7 +37,26 @@ public class BuildMonitorView extends ListView {
         }
     }
 
-    public List<JobView> getJobs() {
+    /**
+     * Because of how org.kohsuke.stapler.HttpResponseRenderer is implemented
+     * it can only work with net.sf.JSONObject in order to produce correct application/json
+     * output
+     *
+     * @return
+     * @throws Exception
+     */
+    @JavaScriptMethod
+    public JSONObject fetchJobViews() throws Exception {
+        ObjectMapper m = new ObjectMapper();
+
+        return asJsonObject("{jobs:" + m.writeValueAsString(jobViews()) + "}");
+    }
+
+    private JSONObject asJsonObject(String jsonString) {
+        return (JSONObject) JSONSerializer.toJSON(jsonString);
+    }
+
+    private List<JobView> jobViews() {
         List<JobView> jobs = new ArrayList<JobView>();
 
         for (TopLevelItem item : super.getItems()) {
