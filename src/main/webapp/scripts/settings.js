adf56e8900cff2e78ae6f333745670aed7c578d4
jan-molak <jan.molak@gmail.com>
2014-04-05 12:40:21 +0100

    Feature: Build Monitor supports colour-blind mode
    Closes #30

--- src/main/webapp/scripts/settings.js
+++ src/main/webapp/scripts/settings.js
@@ -7,6 +7,7 @@ angular.
 
             $scope.settings.fontSize        = cookieJar.get('fontSize',        1);
             $scope.settings.numberOfColumns = cookieJar.get('numberOfColumns', 2);
+            $scope.settings.colourBlind     = cookieJar.get('colourBlind',     0);
 
             angular.forEach($scope.settings, function(value, name) {
                 $scope.$watch('settings.' + name, function(currentValue) {
