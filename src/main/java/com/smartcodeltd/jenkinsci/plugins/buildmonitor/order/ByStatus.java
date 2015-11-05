fdb6d40893b594d9d2c803cf0b5ff54e8451a604
jan-molak <jan.molak@gmail.com>
2015-11-05 21:38:57 +0000

    (fix) Build Monitor no longer throws a NPE when the jobs are ordered by status and one or more of them is in progress.
    Fixes #181

--- src/main/java/com/smartcodeltd/jenkinsci/plugins/buildmonitor/order/ByStatus.java
+++ src/main/java/com/smartcodeltd/jenkinsci/plugins/buildmonitor/order/ByStatus.java
@@ -1,7 +1,5 @@
 package com.smartcodeltd.jenkinsci.plugins.buildmonitor.order;
 
-import hudson.model.AbstractBuild;
-import hudson.model.AbstractProject;
 import hudson.model.Job;
 import hudson.model.Result;
 import hudson.model.Run;
@@ -11,33 +9,33 @@ import java.util.Comparator;
 public class ByStatus implements Comparator<Job<?, ?>> {
     @Override
     public int compare(Job<?, ?> a, Job<?, ?> b) {
-        return bothProjectsHaveBuildHistory(a, b) ?
-            compareLastBuilds(a, b) :
-            compareProjects(a, b);
+        return bothProjectsHaveBuildHistory(a, b)
+            ? compareRecentlyCompletedBuilds(a, b)
+            : compareProjects(a, b);
     }
 
     // --
 
     private boolean bothProjectsHaveBuildHistory(Job<?, ?> a, Job<?, ?> b) {
-        return a.getLastBuild() != null && b.getLastBuild() != null;
+        return a.getLastCompletedBuild() != null && b.getLastCompletedBuild() != null;
     }
 
     private int compareProjects(Job<?, ?> a, Job<?, ?> b) {
-        Run<?, ?> lastBuildOfA = a.getLastBuild();
-        Run<?, ?> lastBuildOfB = b.getLastBuild();
+        Run<?, ?> recentBuildOfA = a.getLastCompletedBuild();
+        Run<?, ?> recentBuildOfB = b.getLastCompletedBuild();
 
-        if (lastBuildOfA == null && lastBuildOfB != null) {
+        if (recentBuildOfA == null && recentBuildOfB != null) {
             return -1;
-        } else if (lastBuildOfA != null && lastBuildOfB == null) {
+        } else if (recentBuildOfA != null && recentBuildOfB == null) {
             return 1;
         } else {
             return 0;
         }
     }
 
-    private int compareLastBuilds(Job<?, ?> a, Job<?, ?> b) {
-        Result lastResultOfA = a.getLastBuild().getResult();
-        Result lastResultOfB = b.getLastBuild().getResult();
+    private int compareRecentlyCompletedBuilds(Job<?, ?> a, Job<?, ?> b) {
+        Result lastResultOfA = a.getLastCompletedBuild().getResult();
+        Result lastResultOfB = b.getLastCompletedBuild().getResult();
 
         if (lastResultOfA.isWorseThan(lastResultOfB)) {
             return -1;
