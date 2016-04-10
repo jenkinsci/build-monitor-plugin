e112395bb8e806a4efebf3b79fff34d6d274f5d9
jan-molak <jan.molak@gmail.com>
2016-04-11 00:59:45 +0100

    Refactoring: A bit of wordsmithing + simplified the ByLastBuildTime comparator so that it doesn't have to multiply the result by a constant.
    Closes #113
    Closes #186

--- build-monitor-plugin/src/main/java/com/smartcodeltd/jenkinsci/plugins/buildmonitor/order/ByLastBuildTime.java
+++ build-monitor-plugin/src/main/java/com/smartcodeltd/jenkinsci/plugins/buildmonitor/order/ByLastBuildTime.java
@@ -5,30 +5,30 @@ import hudson.model.Job;
 import java.util.Comparator;
 
 /**
- * Inspired in <a href="https://github.com/Mercynary">@Mercenary</a>'s answer to
+ * Inspired by <a href="https://github.com/Mercynary">@Mercenary</a>'s answer to
  * issue <a href="https://github.com/jan-molak/jenkins-build-monitor-plugin/issues/113">#113</a>.
  */
-public class ByLastBuild implements Comparator<Job<?, ?>> {
+public class ByLastBuildTime implements Comparator<Job<?, ?>> {
 
     @Override
     public int compare(Job<?, ?> a, Job<?, ?> b) {
 
-    	// none of then executed
+    	// none of them executed
     	if (a.getLastBuild() == null && b.getLastBuild() == null) {
     		return 0;
     	}
 
-    	// just job B executed, so job A it's older than job B
+    	// only job B executed, so job A is older than job B
     	if (a.getLastBuild() == null && b.getLastBuild() != null) {
     		return 1;
     	}
 
-    	// just job A executed, so job A it's newer than Job B
+    	// only job A executed, so job A is newer than Job B
     	if (a.getLastBuild() != null && b.getLastBuild() == null) {
     		return -1;
     	}
 
-    	// ordering from newer to older
-        return (a.getLastBuild().getTimestamp().compareTo(b.getLastBuild().getTimestamp()) * -1);
+    	// "New is always better" - B. Stinson ;-)
+        return (b.getLastBuild().getTimestamp().compareTo(a.getLastBuild().getTimestamp()));
     }
 }
