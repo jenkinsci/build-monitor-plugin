473f2cc506ee2f66b323f66baa051abe28c99f78
Daniel Beland <dcendents@gmail.com>
2016-12-09 23:15:25 -0500

    Listing all current (building) builds on the job widget

--- build-monitor-plugin/src/main/webapp/scripts/expansions/build-time.js
+++ build-monitor-plugin/src/main/webapp/scripts/expansions/build-time.js
@@ -13,14 +13,14 @@ directive('buildTime', [function () {
         },
         template: [
             '<div>',
-                '<div data-ng-show="!!project.estimatedDuration && project.progress > 0" class="build-time">',
-                    '<span class="elapsed" title="Elapsed time">{{project.lastBuild.duration}}</span>',
+                '<div data-ng-show="!!project.estimatedDuration && project.progress > 0" data-ng-repeat="build in project.currentBuilds track by $index" class="build-time">',
+                    '<span class="elapsed" title="Elapsed time">{{build.duration}}</span>',
                     ' | ',
                     '<span class="estimated" title="Typical build time">{{project.estimatedDuration}}</span>',
                 '</div>',
 
-                '<div data-ng-show="!!project.lastBuild.timeElapsedSince && project.progress == 0" class="build-time">',
-                    '<span class="estimated" title="Last execution">{{project.lastBuild.timeElapsedSince | estimatedTimeElapsedSince}}</span>',
+                '<div data-ng-show="!!project.lastCompletedBuild.timeElapsedSince && project.progress == 0" class="build-time">',
+                    '<span class="estimated" title="Last execution">{{project.lastCompletedBuild.timeElapsedSince | estimatedTimeElapsedSince}}</span>',
                 '</div>',
             '</div>',
         ].join('\n')
---
+++ build-monitor-plugin/src/test/java/com/smartcodeltd/jenkinsci/plugins/buildmonitor/viewmodel/features/KnowsLastCompletedBuildDetailsTest.java
@@ -0,0 +1,69 @@
+package com.smartcodeltd.jenkinsci.plugins.buildmonitor.viewmodel.features;
+
+import com.smartcodeltd.jenkinsci.plugins.buildmonitor.viewmodel.JobView;
+import org.junit.Test;
+
+import static com.smartcodeltd.jenkinsci.plugins.buildmonitor.viewmodel.syntacticsugar.Sugar.*;
+import static com.smartcodeltd.jenkinsci.plugins.buildmonitor.viewmodel.syntacticsugar.TimeMachine.currentTime;
+import static hudson.model.Result.SUCCESS;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class KnowsLastCompletedBuildDetailsTest {
+    private JobView view;
+
+    @Test
+    public void should_know_current_build_number() {
+        view = a(jobView().which(new KnowsLastCompletedBuildDetails()).of(
+                a(job().whereTheLast(build().hasNumber(5)))));
+
+        assertThat(lastCompletedBuildOf(view).name(), is("#5"));
+    }
+
+    @Test
+    public void should_use_build_name_if_its_known() {
+        view = a(jobView().which(new KnowsLastCompletedBuildDetails()).of(
+                a(job().whereTheLast(build().hasName("1.3.4+build.15")))));
+
+        assertThat(lastCompletedBuildOf(view).name(), is("1.3.4+build.15"));
+    }
+
+    @Test
+    public void should_know_the_url_of_the_last_build() {
+        view = a(jobView().which(new KnowsLastCompletedBuildDetails()).of(
+                a(job().whereTheLast(build().hasNumber(22))))
+                .with(locatedAt("job/project-name")));
+
+        assertThat(lastCompletedBuildOf(view).url(), is("job/project-name/22/"));
+    }
+
+    /*
+     * Elapsed time
+     */
+
+    @Test
+    public void should_know_how_long_the_last_build_took_once_its_finished() throws Exception {
+        view = a(jobView().which(new KnowsLastCompletedBuildDetails()).of(
+                a(job().whereTheLast(build().finishedWith(SUCCESS).and().took(3)))));
+
+        assertThat(lastCompletedBuildOf(view).duration(), is("3m 0s"));
+    }
+
+    /*
+     * Last build, last success and last failure (ISO 8601)
+     */
+    @Test
+    public void should_know_how_long_since_the_last_build_happened() throws Exception {
+        String tenMinutesInMilliseconds = String.format("%d", 10 * 60 * 1000);
+
+        view = a(jobView().which(new KnowsLastCompletedBuildDetails()).of(
+                a(job().whereTheLast(build().startedAt("18:05:00").and().took(5))))
+                .assuming(currentTime().is("18:20:00")));
+
+        assertThat(lastCompletedBuildOf(view).timeElapsedSince(), is(tenMinutesInMilliseconds));
+    }
+
+    private KnowsLastCompletedBuildDetails.LastCompletedBuild lastCompletedBuildOf(JobView job) {
+        return job.which(KnowsLastCompletedBuildDetails.class).asJson();
+    }
+}
