e8b0f2044134ac648e6e218ee921b354fcaef8ed
vincent <vincent.grenfeldt@gmail.com>
2017-04-27 11:35:26 +0200

    bugfixes

--- build-monitor-plugin/src/main/webapp/scripts/settings.js
+++ build-monitor-plugin/src/main/webapp/scripts/settings.js
@@ -11,7 +11,7 @@ angular.
             $scope.settings.reduceMotion    = cookieJar.get('reduceMotion',    0);
             $scope.settings.showBadges      = cookieJar.get('showBadges',      0);
             $scope.settings.showQueueStatus         = cookieJar.get('showQueueStatus',                      1);
-                        $scope.settings.notifyIfBuildTimeExceedsEstimated         = cookieJar.get('notifyIfBuildTimeExceedsEstimated',    1);
+            $scope.settings.notifyIfBuildTimeExceedsEstimated         = cookieJar.get('notifyIfBuildTimeExceedsEstimated',    1);
             angular.forEach($scope.settings, function(value, name) {
                 $scope.$watch('settings.' + name, function(currentValue) {
                     cookieJar.put(name, currentValue);
