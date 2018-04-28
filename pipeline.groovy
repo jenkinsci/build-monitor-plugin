5b488065f1aca4d073cb089499eee61c89561062
jan-molak <jan.molak@gmail.com>
2018-04-28 18:17:49 +0100

    Upgrading maven to 3.5.3, thanks @erikhakansson

--- pipeline.groovy
+++ pipeline.groovy
@@ -6,7 +6,7 @@ node('hi-speed') {
 
     git url: 'git@github.com:jan-molak/jenkins-build-monitor-plugin.git', branch: 'master'
 
-    withEnv(["JAVA_HOME=${ tool 'jdk-1.8.latest' }", "M2_HOME=${tool 'maven-3.2.5'}", "PATH+EXTRA=${env.M2_HOME}/bin:${env.JAVA_HOME}/bin:${tool 'node-8.11.1'}/bin"]) {
+    withEnv(["JAVA_HOME=${ tool 'jdk-1.8.latest' }", "M2_HOME=${tool 'maven-3.5.3'}", "PATH+EXTRA=${env.M2_HOME}/bin:${env.JAVA_HOME}/bin:${tool 'node-8.11.1'}/bin"]) {
 
         mvn "-version"
 
@@ -29,7 +29,7 @@ node('hi-speed') {
     unstash 'sources'
 
     with_browser_stack 'linux-x64', {
-        withEnv(["JAVA_HOME=${ tool 'jdk-1.8.latest' }", "M2_HOME=${tool 'maven-3.2.5'}", "PATH+EXTRA=${env.M2_HOME}/bin:${env.JAVA_HOME}/bin"]) {
+        withEnv(["JAVA_HOME=${ tool 'jdk-1.8.latest' }", "M2_HOME=${tool 'maven-3.5.3'}", "PATH+EXTRA=${env.M2_HOME}/bin:${env.JAVA_HOME}/bin"]) {
 
             mvn "-version"
 
