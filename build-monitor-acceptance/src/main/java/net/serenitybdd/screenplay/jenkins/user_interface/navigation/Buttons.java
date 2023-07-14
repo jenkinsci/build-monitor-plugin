547a5a94d1171bca888541d12629c7aa1bbb23c1
Basil Crow <me@basilcrow.com>
2023-07-14 16:10:26 -0700

    Adapt to core UI changes

--- build-monitor-acceptance/src/main/java/net/serenitybdd/screenplay/jenkins/user_interface/navigation/Buttons.java
+++ build-monitor-acceptance/src/main/java/net/serenitybdd/screenplay/jenkins/user_interface/navigation/Buttons.java
@@ -5,7 +5,7 @@ import net.serenitybdd.screenplay.targets.Target;
 
 public class Buttons {
     public static final Target Create =
-            Target.the("the 'Create' button").locatedBy("//input[@name='Submit' and @value='Create']");
+            Target.the("the 'Create' button").locatedBy("//button[@name='Submit']");
     public static final Target Save = Button.called("Save");
     public static final Target OK = Button.called("OK");
 }
