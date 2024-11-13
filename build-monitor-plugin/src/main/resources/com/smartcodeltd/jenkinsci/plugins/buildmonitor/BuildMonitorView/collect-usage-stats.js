f648f11eb046968b98c6cfa4948d8ec576bfd12f
Yaroslav Afenkin <91559310+yaroslavafenkin@users.noreply.github.com>
2024-11-13 12:49:48 +0200

    Address https://github.com/jenkinsci/build-monitor-plugin/pull/1034#discussion_r1838516617

--- build-monitor-plugin/src/main/resources/com/smartcodeltd/jenkinsci/plugins/buildmonitor/BuildMonitorView/collect-usage-stats.js
+++ build-monitor-plugin/src/main/resources/com/smartcodeltd/jenkinsci/plugins/buildmonitor/BuildMonitorView/collect-usage-stats.js
@@ -1,31 +1,33 @@
-window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+window.addEventListener("DOMContentLoaded", () => {
+    window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
 
-const {
-    buildMonitorVersion,
-    jenkinsVersion,
-    installationSize,
-    itemsSize,
-    installationAudience,
-    anonymousCorrelationId
-} = document.querySelector('.build-monitor-ga-data-holder').dataset;
+    const {
+        buildMonitorVersion,
+        jenkinsVersion,
+        installationSize,
+        itemsSize,
+        installationAudience,
+        anonymousCorrelationId
+    } = document.querySelector('.build-monitor-ga-data-holder').dataset;
 
-ga('create', 'UA-61694827-4', 'auto', {
-    'userId': anonymousCorrelationId,
-    'sampleRate': 1
+    ga('create', 'UA-61694827-4', 'auto', {
+        'userId': anonymousCorrelationId,
+        'sampleRate': 1
+    });
+
+    ga('set', {
+        'forceSSL': true,
+        'appName': 'Build Monitor',
+        'appId': 'build-monitor-plugin',
+
+        'appVersion': buildMonitorVersion,
+        'appInstallerId': jenkinsVersion,
+
+        'dimension1': installationSize,
+        'dimension2': itemsSize,
+        'dimension3': installationAudience,
+        'dimension4': anonymousCorrelationId
+    });
+
+    ga('send', 'screenview', {screenName: 'Dashboard'});
 });
-
-ga('set', {
-    'forceSSL': true,
-    'appName': 'Build Monitor',
-    'appId': 'build-monitor-plugin',
-
-    'appVersion': buildMonitorVersion,
-    'appInstallerId': jenkinsVersion,
-
-    'dimension1': installationSize,
-    'dimension2': itemsSize,
-    'dimension3': installationAudience,
-    'dimension4': anonymousCorrelationId
-});
-
-ga('send', 'screenview', {screenName: 'Dashboard'});
