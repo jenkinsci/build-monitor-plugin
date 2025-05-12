6770ba9fe1bdab3a4471b83c0e1f0c7eecc47e30
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-05-12 10:28:45 +0100

    Tidy

--- src/main/frontend/components/label.tsx
+++ src/main/frontend/components/label.tsx
@@ -1,34 +1,9 @@
-import { CSSProperties, useEffect, useState } from "react";
+import { CSSProperties } from "react";
+import TextTransition from "react-text-transition";
 
 function Label({ text, style = {} }: { text: string; style?: CSSProperties }) {
-  const [oldText, setOldText] = useState(text);
-  const [animating, setAnimating] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      if (oldText !== text) {
-        setAnimating(true);
-        setTimeout(() => {
-          setOldText(text);
-          setAnimating(false);
-        }, 400);
-      }
-    }, 200);
-  }, [text]);
-
   return (
-    <div className={"shineofthesun"} style={{ position: "relative", ...style }}>
-      <span
-        className={"lablething1 " + (animating ? "lablething1animating" : "")}
-      >
-        {oldText}
-      </span>
-      <span
-        className={"lablething2 " + (animating ? "lablething2animating" : "")}
-      >
-        {text}
-      </span>
-    </div>
+    <TextTransition style={{ position: "relative", ...style }}>{text}</TextTransition>
   );
 }
 
