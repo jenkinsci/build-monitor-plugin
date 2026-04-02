2cfd0ddb45ffa8ce4d0ee9b6a55525a4e3a8032c
shaidshark <shaidshark@users.noreply.github.com>
2026-04-02 15:02:47 +0200

    Fix multibranch pipeline test: add @UsePlaywright, fix reflection, rename to PascalCase
    - Added @UsePlaywright(PlaywrightConfig.class) annotation to enable Playwright Page injection
    - Fixed reflection code to use Item.class instead of Job.class and call addLoadedChild from AbstractFolder
    - Renamed test file from lowercase to PascalCase: ShouldSupportMultibranchPipelinesTest.java
    - Fixed pipeline syntax (missing closing brace)
    - Changed job separator from » to / to match actual display format
    - Updated to use static import for createBuildMonitorView

--- src/test/java/com/smartcodeltd/jenkinsci/plugins/buildmonitor/e2e/ShouldSupportMultibranchPipelinesTest.java
+++ src/test/java/com/smartcodeltd/jenkinsci/plugins/buildmonitor/e2e/ShouldSupportMultibranchPipelinesTest.java
@@ -1,9 +1,12 @@
 package com.smartcodeltd.jenkinsci.plugins.buildmonitor.e2e;
 
+import static com.smartcodeltd.jenkinsci.plugins.buildmonitor.e2e.utils.BuildMonitorViewUtils.createBuildMonitorView;
+
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.junit.UsePlaywright;
+import com.smartcodeltd.jenkinsci.plugins.buildmonitor.e2e.config.PlaywrightConfig;
 import com.smartcodeltd.jenkinsci.plugins.buildmonitor.e2e.pages.BuildMonitorViewPage;
-import com.smartcodeltd.jenkinsci.plugins.buildmonitor.e2e.utils.BuildMonitorViewUtils;
-import hudson.model.Job;
+import hudson.model.Item;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject;
@@ -11,9 +14,8 @@ import org.junit.jupiter.api.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 @WithJenkins
+@UsePlaywright(PlaywrightConfig.class)
 class ShouldSupportMultibranchPipelinesTest {
 
     @Test
@@ -23,14 +25,14 @@ class ShouldSupportMultibranchPipelinesTest {
 
         // Create branch jobs and add them to the multibranch project
         WorkflowJob branch1 = new WorkflowJob(multibranch, "master");
-        branch1.setDefinition(new CpsFlowDefinition("pipeline { stages { stage('Test') { steps { echo 'Hello' } } }", true));
-        // Add branch via reflection (add method may be non-public)
-        java.lang.reflect.Method addMethod = WorkflowMultiBranchProject.class.getDeclaredMethod("add", Job.class);
+        branch1.setDefinition(new CpsFlowDefinition("pipeline { stages { stage('Test') { steps { echo 'Hello' } } } }", true));
+        // Add branch via reflection (addLoadedChild is protected in AbstractFolder)
+        java.lang.reflect.Method addMethod = multibranch.getClass().getSuperclass().getSuperclass().getDeclaredMethod("addLoadedChild", Item.class);
         addMethod.setAccessible(true);
         addMethod.invoke(multibranch, branch1);
 
         WorkflowJob branch2 = new WorkflowJob(multibranch, "develop");
-        branch2.setDefinition(new CpsFlowDefinition("pipeline { stages { stage('Test') { steps { echo 'Hello' } } }", true));
+        branch2.setDefinition(new CpsFlowDefinition("pipeline { stages { stage('Test') { steps { echo 'Hello' } } } }", true));
         addMethod.invoke(multibranch, branch2);
 
         // Save the projects to persist state
@@ -39,13 +41,12 @@ class ShouldSupportMultibranchPipelinesTest {
         branch2.save();
 
         // Create a BuildMonitorView that includes the multibranch project (without recursion)
-        var view = BuildMonitorViewUtils.createBuildMonitorView(j, "Build Monitor")
-                .addJobs(multibranch);
+        var view = createBuildMonitorView(j, "Build Monitor").addJobs(multibranch);
 
         // Verify that the branch jobs appear in the monitor view
-        BuildMonitorViewPage.from(p, view.get())
+        BuildMonitorViewPage.from(p, view)
                 .goTo()
-                .hasJob("MyMultibranch » master")
-                .hasJob("MyMultibranch » develop");
+                .hasJob("MyMultibranch / master")
+                .hasJob("MyMultibranch / develop");
     }
 }
