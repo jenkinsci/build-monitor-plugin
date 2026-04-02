ef3ed07ba94edb6f28d6d23533f006d0425e47c4
shaidshark <shaidshark@users.noreply.github.com>
2026-04-03 01:48:24 +0200

    fix: use createProject API instead of reflection for multibranch test
    Replace fragile reflection-based addLoadedChild call with the proper
    Jenkins createProject API for creating branch jobs in multibranch
    pipelines. This is more robust against internal Jenkins API changes.

--- src/test/java/com/smartcodeltd/jenkinsci/plugins/buildmonitor/e2e/ShouldSupportMultibranchPipelinesTest.java
+++ src/test/java/com/smartcodeltd/jenkinsci/plugins/buildmonitor/e2e/ShouldSupportMultibranchPipelinesTest.java
@@ -7,6 +7,11 @@ import com.microsoft.playwright.junit.UsePlaywright;
 import com.smartcodeltd.jenkinsci.plugins.buildmonitor.e2e.config.PlaywrightConfig;
 import com.smartcodeltd.jenkinsci.plugins.buildmonitor.e2e.pages.BuildMonitorViewPage;
 import hudson.model.Item;
+import hudson.model.ItemGroup;
+import hudson.model.TopLevelItem;
+import java.util.ArrayList;
+import java.util.List;
+import jenkins.branch.MultiBranchProject;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject;
@@ -23,22 +28,16 @@ class ShouldSupportMultibranchPipelinesTest {
         // Create a multibranch pipeline project
         WorkflowMultiBranchProject multibranch = j.createProject(WorkflowMultiBranchProject.class, "MyMultibranch");
 
-        // Create branch jobs and add them to the multibranch project
-        WorkflowJob branch1 = new WorkflowJob(multibranch, "master");
+        // Create branch jobs and register them properly through the Jenkins API
+        WorkflowJob branch1 = multibranch.createProject(WorkflowJob.class, "master");
         branch1.setDefinition(new CpsFlowDefinition("pipeline { stages { stage('Test') { steps { echo 'Hello' } } } }", true));
-        // Add branch via reflection (addLoadedChild is protected in AbstractFolder)
-        java.lang.reflect.Method addMethod = multibranch.getClass().getSuperclass().getSuperclass().getDeclaredMethod("addLoadedChild", Item.class);
-        addMethod.setAccessible(true);
-        addMethod.invoke(multibranch, branch1);
-
-        WorkflowJob branch2 = new WorkflowJob(multibranch, "develop");
-        branch2.setDefinition(new CpsFlowDefinition("pipeline { stages { stage('Test') { steps { echo 'Hello' } } } }", true));
-        addMethod.invoke(multibranch, branch2);
-
-        // Save the projects to persist state
-        multibranch.save();
         branch1.save();
+
+        WorkflowJob branch2 = multibranch.createProject(WorkflowJob.class, "develop");
+        branch2.setDefinition(new CpsFlowDefinition("pipeline { stages { stage('Test') { steps { echo 'Hello' } } } }", true));
         branch2.save();
+
+        multibranch.save();
 
         // Create a BuildMonitorView that includes the multibranch project (without recursion)
         var view = createBuildMonitorView(j, "Build Monitor").addJobs(multibranch);
